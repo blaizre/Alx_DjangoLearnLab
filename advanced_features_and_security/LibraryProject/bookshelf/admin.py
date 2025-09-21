1bbe247ff1a7a0c0dcbb690feaae434ed9f932b6
blaizre <jlego104@gmail.com>
2025-09-21 03:51:00 -0400

    Advanced Features And Security.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from .models import Book
+from .models import Book, CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -9,3 +10,15 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year', 'author')            # Add filter sidebar by year and author
     search_fields = ('title', 'author')                     # Add search box for title and author
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ["username", "email", "is_staff", "is_active", "role"]
+    list_filter = ["is_staff", "is_active", "role"]
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {"fields": ("role",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+            (None, {"fields": ("role",)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
