1ee738d071f354770589ac32f2204612df9e2a1f
blaizre <jlego104@gmail.com>
2025-09-21 10:11:20 -0400

    Setup new Django REST Framework.

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, default="Unknown")
 
     def __str__(self):
         return self.title
