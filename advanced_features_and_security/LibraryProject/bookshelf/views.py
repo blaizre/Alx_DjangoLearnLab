cddff0000c74781fda75b3f3fbcd2bf9b1fc1dbf
blaizre <jlego104@gmail.com>
2025-09-21 06:23:22 -0400

    Update views.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-
+from .forms import ExampleForm
 # Create your views here.
 
 #Only users with 'can_view' permission can see this view.
